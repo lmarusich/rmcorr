cb681318001419d9aa53f7d40eaf2e3acc716607
lmarusich <lmarusich@gmail.com>
2015-07-24 13:56:03 -0500

    Update

--- R/rmcorr.R
+++ R/rmcorr.R
@@ -4,10 +4,15 @@
 #' @param measure1 A numeric variable giving the observations for one measure.
 #' @param measure2 A numeric variable giving the observations for the second measure.
 #' @param dataset The data frame containing the variables.
+#' @param CIs The method of calculating confidence intervals.
+#' @param bstrap.out Determines if the output include the bootstrap resamples.
+#' @param nreps The number of resamples to take if bootstrapping.
 #' @return A list with class "rmc" containing the following components.
 #' \item{r}{the value of the repeated measures correlation coefficient.}
 #' \item{CI}{the 95\% confidence interval for the repeated measures correlation coefficient.}
 #' \item{p}{the p-value for the repeated measures correlation coefficient.}
+#' \item{model}{the multiple regression model used to calculate the correlation coefficient.}
+#' \item{resamples}{the bootstrap resampled correlation values.}
 #' @seealso \code{\link{rmcplot}}
 #' @examples
 #' ## Bland Altman 1995 data
@@ -17,7 +22,7 @@
 
 rmcorr <- function(participant, measure1, measure2, dataset, 
                    CIs = c("analytic", "bootstrap"), 
-                   pval = c("analytic", "bootstrap")) {
+                   bstrap.out = F, nreps = NULL) {
     
     options(contrasts = c("contr.sum", "contr.poly"))
     
@@ -54,21 +59,47 @@ rmcorr <- function(participant, measure1, measure2, dataset,
     rmcorrvalue <- as.numeric(corrsign * sqrt(SSFactor / (SSFactor + SSresidual)))
     
     # Pvalue and confidence intervals
+    pvalue <- type3rmcorr$'Pr(>F)'[3]
     
     #analytic
-    if (CIs == "analytic"){}
-     
+    if (CIs == "analytic"){
+        rmcorrvalueCI <- psych::r.con(rmcorrvalue, errordf) 
+    } else if (CIs == "bootstrap") {
+        nsubs <- length(levels(Participant))
+        if (is.null(nreps)){stop("Specify the number of bootstrap resamples to take")}
+        cor.reps <- numeric(nreps)
+        
+        for (i in 1:nreps){
+            bs.1 <- NULL
+            bs.2 <- NULL
+            
+            for (j in 1:nsubs) {
+                subdata <- which(Participant==levels(Participant)[j])
+                resamp <- sample(subdata,length(subdata),replace=T)
+                bs.1 <- c(bs.1,Measure1[resamp])
+                bs.2 <- c(bs.2,Measure2[resamp])
+            }
+            
+            bs.Part <- Participant
+            repmodel<-lm(bs.1 ~ bs.Part + bs.2)
+            repslope <- coef(repmodel)["bs.2"]
+            errordf <- repmodel$df.residual
+            repsign <- sign(repslope)  
+            
+            type3rmcorr<-drop1(repmodel, ~., test="F" )
+            SSFactor<-type3rmcorr$'Sum of Sq'[3] 
+            SSresidual<-type3rmcorr$RSS[1]
+            
+            cor.reps[i] <- as.numeric(repsign*sqrt(SSFactor/(SSFactor+SSresidual)))
+        }
+        rmcorrvalueCI <- quantile(cor.reps,probs=c(.025,.975))
+    }
     
-    pvalue <- type3rmcorr$'Pr(>F)'[3]
-    rmcorrvalueCI <- psych::r.con(rmcorrvalue, errordf)  
-    
-    #bootstrapped confidence intervals
-    
-    #bootstrapped p-value
-    
+    resamples <- ifelse(CIs == "boostrap",cor.reps,NULL)
     
     rmoutput <- list(r = rmcorrvalue, CI = rmcorrvalueCI, p = pvalue, 
-                     model = lmmodel)
+                     model = lmmodel, resamples = resamples)
+    if (bstrap.out) {rmoutput$resamples <- resamples}
     class(rmoutput) <- "rmc"
     return (rmoutput)
 } 
