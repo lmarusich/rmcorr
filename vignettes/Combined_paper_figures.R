d73ff97c98b8e7187e90588b797bfc84ea43a8d0
lmarusich <lmarusich@gmail.com>
2017-08-22 12:24:25 -0500

    Moving dependencies for vignettes to "Suggests" from "Imports"

--- vignettes/Combined_paper_figures.R
+++ vignettes/Combined_paper_figures.R
@@ -3,13 +3,12 @@ require(pwr)
 require(psych) 
 require(RColorBrewer)
 require(plotrix)
-library(rmcorr)
+require(rmcorr)
+require(lme4)
+require(AICcmodavg)
+require(ggplot2)
+require(merTools)
 
-#install_github('lmarusich/rmcorr')
-
-#require(checkpoint)
-#checkpoint("2016-XX-YY") #Won't work yet b/c rmcorr doesn't yet exist in CRAN
-#require(memoise)
 addalpha <- function(colors, alpha=1.0) {
     r <- col2rgb(colors, alpha=T)
     # Apply alpha
@@ -68,20 +67,19 @@ cols <- mypal(10)
 
 par(mfrow = c(1, 2), mgp = c(2.5, .75, 0), mar = c(4,4,2,1), cex = 1.2)
 
-plot(example.rmc, exampleMat, xlab = "x", ylab = "y", main = "a) Rmcorr Plot",
+plot(example.rmc, exampleMat, xlab = "x", ylab = "y", 
      overall = F, palette = mypal, las = 1, ylim = c(-6, 6.5))
 text(1,-5,paste('rmcorr =', round(example.rmc$r,2),'\np < 0.001'), adj = 1)
 
 plot(submeanx[,2], submeany[,2], pch = 16, col = cols, las = 1,
-     main = "b) Regression Plot", xlab = "x (averaged for each participant)",
+     xlab = "x (averaged for each participant)",
      ylab = "y (averaged for each participant)", ylim=c(-6,6.5), xlim=c(-3, 1))
 stdr <- cor.test(submeanx[,2], submeany[,2])
 text(1,-5,paste('r =', round(stdr$est,2),'\np =', round(stdr$p.value,2)), adj = 1)
 abline(lm(submeany[,2]~submeanx[,2]),col="gray50")
-
-dev.copy(pdf, file="plots/Figure1_Rmcorr_vs_reg.pdf", 9, 6)
+print(getwd())
+dev.copy2eps(file="plots/Figure1_Rmcorr_vs_reg.eps", height = 6, width = 8)
 dev.off()
-
 
 ## ---- echo = FALSE, warning = FALSE, results = "hide", fig.height = 9, fig.width = 9----
 par(mfrow = c(3,3), mar = c(1,1,.5,.5), mgp = c(2.5,.75,0), 
@@ -137,12 +135,12 @@ makeminiplot(subxs, .9, -4.6, 12, xax = "s")
 
 mtext(side = 1, outer = T, line = 1.5, "x", at = c(.175, .5, .85))
 mtext(side = 2, outer = T, line = 1.5, "y", at = c(.175, .5, .85), las = 1)
-mtext(side = 3, outer = T, line = .5, 
-      c("a) rmcorr = -1", "b) rmcorr = 0", "c) rmcorr = 1"),
-      at = c(.175, .5, .85), las = 1, cex = 1.5)
+# mtext(side = 3, outer = T, line = .5, 
+#       c("a) rmcorr = -1", "b) rmcorr = 0", "c) rmcorr = 1"),
+#       at = c(.175, .5, .85), las = 1, cex = 1.5)
 
 
-dev.copy(pdf, file="plots/Figure2_Rmcorr_vs_OLS.pdf", 9, 9)
+dev.copy2eps(file="plots/Figure2_Rmcorr_vs_OLS.eps")
 dev.off()
 
 
@@ -175,7 +173,7 @@ example.rmc <- rmcorr(sub,newX,newY,exampleMat)
 
 mypal <- colorRampPalette(RColorBrewer::brewer.pal(10,'Paired'))
 
-plot(example.rmc, exampleMat, xlab = "x", ylab = "", main = "a) Original",
+plot(example.rmc, exampleMat, xlab = "x", ylab = "", 
      overall = F, palette = mypal, xlim = c(-3.5, 1), ylim = c(-2.5,2), las = 1)
 text(-3.5,1.75,paste('rmcorr =', round(example.rmc$r,2),'\np < 0.001'), adj = 0)
 mtext(side = 2, "y", las = 1, line = 2.5, cex = 1.2)
@@ -184,7 +182,7 @@ mtext(side = 2, "y", las = 1, line = 2.5, cex = 1.2)
 exampleMat2 <- exampleMat
 exampleMat2$newX <- exampleMat2$newX * .5 + 1
 example2.rmc <- rmcorr(sub, newX, newY, exampleMat2)
-plot(example2.rmc, exampleMat2, xlab = "x", ylab = "", main = "b) x / 2 + 1", overall = F,
+plot(example2.rmc, exampleMat2, xlab = "x", ylab = "", overall = F,
      palette = mypal, xlim = c(-3.5, 1), ylim = c(-2.5,2), las = 1)
 text(-3.5,1.75,paste('rmcorr =', round(example2.rmc$r,2),'\np < 0.001'), adj = 0)
 mtext(side = 2, "y", las = 1, line = 2.5, cex = 1.2)
@@ -193,12 +191,12 @@ mtext(side = 2, "y", las = 1, line = 2.5, cex = 1.2)
 exampleMat3 <- exampleMat
 exampleMat3$newY[11:15] <- exampleMat3$newY[11:15] - 2
 example3.rmc <- rmcorr(sub, newX, newY, exampleMat3)
-plot(example3.rmc, exampleMat3, xlab = "x", ylab = "", main = "c) Subject 3: y - 2", overall = F,
+plot(example3.rmc, exampleMat3, xlab = "x", ylab = "", overall = F,
      palette = mypal, xlim = c(-3.5, 1), ylim = c(-2.5,2), las = 1)
 text(-3.5,1.75,paste('rmcorr =', round(example3.rmc$r,2),'\np < 0.001'), adj = 0)
 mtext(side = 2, "y", las = 1, line = 2.5, cex = 1.2)
 
-dev.copy(pdf, file="plots/Figure3_Transformations.pdf", 12, 6)
+dev.copy2eps(file="plots/Figure3_Transformations.eps")
 dev.off()
 
 ## ---- echo=FALSE, fig.height = 8, fig.width = 8--------------------------
@@ -222,7 +220,6 @@ bluecolors<-c("#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#084594")
 
 
 plot(nvals, seq(0,1, length.out=length(nvals)), 
-     main = expression(bold("a)")~bolditalic('r'[rm])~bold(and)~bolditalic(r)~bold('=')~ bold('0.10')), 
      xlab=expression(Sample~Size~"("*italic('N')*")"),
      yaxt = "n", ylab = "Power", las = 1, col = "white", 
      xlim=c(0,300))
@@ -250,7 +247,6 @@ greencolors<-c("#c7e9c0","#a1d99b","#74c476","#41ab5d","#238b45","#005a32")
 #orangecols3<-c(orangecols[2],orangecols[3],orangecols[5],orangecols[7],orangecols[9])
 
 plot(nvals, seq(0,1, length.out=length(nvals)), 
-     main = expression(bold("b)")~bolditalic('r'[rm])~bold(and)~bolditalic(r)~bold('=')~ bold('0.30')), 
      xlab=expression(Sample~Size~"("*italic('N')*")"),
      yaxt = "n", ylab = "Power", las = 1, col = "white", 
      xlim=c(0,50))
@@ -276,7 +272,6 @@ powPearsonlarge <- sapply(nvals, function (x) pwr.r.test(n=x, r=0.5)$power)
 purplecolors<-c("#f2f0f7", "#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a51a3", "#4a1486")
 
 plot(nvals, seq(0,1, length.out=length(nvals)), 
-     main = expression(bold("c)")~bolditalic('r'[rm])~bold(and)~bolditalic(r)~bold('=')~ bold('0.50')),
      xlab=expression(Sample~Size~"("*italic('N')*")"),
      yaxt = "n", ylab = "Power", las = 1, col = "white", xlim=c(0,30))
 yLabels <- seq(0, 1, 0.2)
@@ -292,7 +287,7 @@ legend("bottomright", lwd=2, col=purplecolors, legend=c("1", "3", "5", "10", "20
 abline(a = 0.8, b=0, col=1, lty=2, lwd= 2.5)
 lines(nvals, powPearsonlarge, col=purplecolors[2], lwd = 2.5)
 
-dev.copy(pdf, file="plots/Figure4_Power_curves.pdf", 8, 8)
+dev.copy2eps(file="plots/Figure4_Power_curves.eps")
 dev.off()
 
 
@@ -334,8 +329,7 @@ layout(matrix(c(1,3,4,2,3,4), 2, 3, byrow = T))
 #a
 par(mar = c(1,4,4,2), oma = c(0,2,0,0), las = 1)
 plot(Volume ~ Age, data = Time1raz2005, pch = 16,  xlab = "", ylab = "",
-     xlim = c(15,85), ylim = c(105,170), xaxt = "n",
-     main = 'a) Separate Simple\nRegression/Correlations')
+     xlim = c(15,85), ylim = c(105,170), xaxt = "n")
 abline(a1.lm, col = "red", lwd = 2)
 axis.break(axis = 2, style = "slash")
 text(75, 170, "Time 1", cex = 1.5)
@@ -356,19 +350,18 @@ par(mar = c(4,3,4,2))
 blueset <- brewer.pal(8, 'Blues')
 pal <- colorRampPalette(blueset)
 plot(brainvolage.rmc, raz2005, overall = F, palette = pal, ylab = "", xlab = "Age", 
-     cex = 1.2, xlim = c(15,85), ylim = c(105,170), main = 'b) Rmcorr') 
+     cex = 1.2, xlim = c(15,85), ylim = c(105,170)) 
 axis.break(axis = 2, style = "slash")
 text(20,107,paste('rmcorr =', round(brainvolage.rmc$r,2),'\np < 0.001'), adj = 0)
 
 #c
 plot(Volume~Age, data = avgRaz2005, ylab = "", xlab = "Age", cex = 1.2, pch = 16, 
-     main = ("c) Simple Regression/\nCorrelation (Averaged Data)"),
      xlim = c(15,85), ylim = c(105,170))
 abline(brainvolage.lm, col = "red", lwd = 2)
 axis.break(axis = 2, style = "slash")
 text(20,107,paste('r =', round(c.rtest$est,2),'\np < 0.001'), adj = 0)
 
-dev.copy(pdf, file="plots/Figure5_Volume_Age.pdf", 9, 6)
+dev.copy2eps(file="plots/Figure5_Volume_Age.eps")
 dev.off()
 
 
@@ -394,7 +387,7 @@ print(c.rtest)
 par(mfrow=c(1,3), mar=c(5,4.6,4,0.5), mgp=c(3.2,0.8,0),  oma = c(0, 0, 0, 0), las = 1)
 
 plot(vissearch.rmc, gilden2010, overall = F, xlab = "Reaction Time (seconds)", 
-     ylab = "Accuracy", cex = 1.2, main = expression(bold("a)")~bold('Rmcorr')),
+     ylab = "Accuracy", cex = 1.2,
      ylim = c(.79, 1), xlim = c(0.45, .95)) 
 axis.break(axis = 1, style = "slash")
 axis.break(axis = 2, style = "slash")
@@ -402,22 +395,20 @@ text(.95,.8,paste('rmcorr =', round(vissearch.rmc$r,2),'\np =',
                   round(vissearch.rmc$p, 2)), adj = 1)
 
 plot(acc~rt, data = gildenMeans, cex = 1.2, pch = 16, ylim = c(.79, 1), 
-     xlim = c(0.45, .95), xlab = "Reaction Time (seconds)",  ylab = "", 
-     main = ("b) Simple Regression\n(Averaged Data)"))
+     xlim = c(0.45, .95), xlab = "Reaction Time (seconds)",  ylab = "")
 abline(avg.lm, col = "red", lwd = 2)
 axis.break(axis = 1, style = "slash")
 axis.break(axis = 2, style = "slash")
 text(.95,.8,paste('r =', round(b.rtest$est,2),'\np =', round(b.rtest$p.value,2)), adj = 1)
 
 plot(acc~rt, data = gilden2010, xlab = "Reaction Time (seconds)", ylab = "",
-     cex = 1.2, pch = 16, ylim = c(.79, 1), xlim = c(0.45, .95),
-     main = ("c) Simple Regression\n(Aggregated Data)"))
+     cex = 1.2, pch = 16, ylim = c(.79, 1), xlim = c(0.45, .95))
 abline(agg.lm, col = "red", lwd = 2)
 axis.break(axis = 1, style = "slash")
 axis.break(axis = 2, style = "slash")
 text(.95,.8,paste('r =', round(c.rtest$est,2),'\np =', round(c.rtest$p.value,2)), adj = 1)
 
-dev.copy(pdf, file="plots/Figure6_Visual_Search.pdf", 9, 6)
+dev.copy2eps(file="plots/Figure6_Visual_Search.eps")
 dev.off()
 
 
@@ -437,6 +428,131 @@ powvals <- sapply(nvals, function (x) power.rmcorr(k[1], x, 0.5, 0.05)$power)
 powvalsdiff<-as.numeric(powvals) - as.numeric(powPearsonLarge)
 lines(nvals, powvalsdiff, lwd=2.5, col="red")
 
-dev.copy(pdf, file="plots/AppendixA_Figure.pdf", 9, 6) 
+dev.copy2eps(file="plots/AppendixA_Figure.eps") 
 dev.off()
+
+## ---- echo = TRUE--------------------------------------------------------
+brainvolage.rmc <- rmcorr(participant = Participant, measure1 = Age, measure2 = Volume, dataset = raz2005)
+
+#Null multi-level model: Random intercept and fixed slope
+null.vol <- lmer(Volume ~ Age + (1 | Participant), data = raz2005, REML = FALSE)
+
+#Model fit
+null.vol
+
+#Parameter Confidence Intervals
+confint(null.vol)
+
+#Model fitted values and confidence intervals for each participant (L1 effects)
+set.seed(9999)
+L1.predict.raz <- predictInterval(null.vol, newdata = raz2005, n.sims = 1000)
+L1.predict.raz <- cbind(raz2005$Participant, L1.predict.raz)
+
+apatheme =  theme_bw() +
+            theme(panel.grid.major=element_blank(),
+                  panel.grid.minor=element_blank(),
+                  panel.border=element_blank(),
+                  axis.line=element_line(),
+                  legend.position="none",
+                  #text=element_text(family='Times'), #Need- https://github.com/wch/extrafont + Ghostscript
+                  #legend.title=element_blank(),
+                  axis.line.x = element_line(color="black", size = 0.9),
+                  axis.line.y = element_line(color="black", size = 0.9),
+                  axis.text.x = element_text(size = 12),
+                  axis.text.y = element_text(size = 12), 
+                  axis.title.x = element_text(size = 12),
+                  axis.title.y = element_text(size = 12))
+
+#Create custom color palette 
+Blues<-brewer.pal(9,"Blues")
+
+ggplot(raz2005, aes(x = Age, y = Volume, group = Participant, color = Participant)) +
+  geom_line(aes(y = predict(null.vol)), linetype = 2) + 
+  geom_line(aes(y = brainvolage.rmc$model$fitted.values), linetype = 1) + 
+  geom_ribbon(aes(ymin = L1.predict.raz$lwr,
+                  ymax = L1.predict.raz$upr,
+                  group = L1.predict.raz$`raz2005$Participant`,
+                  linetype = NA), alpha = 0.07) +
+  apatheme + 
+  labs(title = "Rmcorr and Random Intercept Multi-Level Model:\n Raz et al. 2005 Data", x = "Age", 
+       y = expression(Cerebellar~Hemisphere~Volume~(cm^{3}))) +
+  geom_point(aes(colour = Participant)) +
+  scale_colour_gradientn(colours=Blues) + theme(plot.title = element_text(hjust = 0.5)) + 
+  geom_abline(intercept = fixef(null.vol)[1], slope = fixef(null.vol)[2], colour = "black", size = 1, linetype = 2)
+
+ggsave(file = "plots/AppendixC_Figure1.eps", width = 5.70 , height = 5.73, dpi = 300)
+
+## ---- echo = TRUE--------------------------------------------------------
+vissearch.rmc <- rmcorr(participant = sub, measure1 = rt, measure2 = acc, dataset = gilden2010)
+
+null.vis <- lmer(acc ~ rt + (1 | sub), data = gilden2010, REML = FALSE)
+
+#Model 1: Random intercept + random slope for RT
+model1.vis <- lmer(acc ~ rt + (1 + rt | sub), data = gilden2010, REML = FALSE)
+
+#Model Comparison
+#a) Chi-Square
+anova(null.vis, model1.vis)
+
+#b) Evidence ratio using AIC
+Models.vis<-list()
+Models.vis<-c(null.vis, model1.vis)
+
+ModelTable2<-aictab(Models.vis, modnames = c("null", "Model 1"))
+ModelTable2
+evidence(ModelTable2)
+
+  #Estimating CIs: Convergence problems with model 1
+  confint(model1.vis)
+  warnings()
+  
+  set.seed(9999)
+  predictInterval(model1.vis, newdata = gilden2010, n.sims = 1000)
+  warnings()
+
+#Estimating and graphing null model 
+#Parameter Confidence Intervals
+confint(null.vis)
+
+#Model fitted values and confidence intervals for each participant (L1 effects)
+set.seed(9999)
+L1.predict.gilden <- predictInterval(null.vis, newdata = gilden2010, n.sims = 1000)
+L1.predict.gilden <- cbind(gilden2010$sub, L1.predict.gilden)
+
+apatheme =  theme_bw() +
+            theme(panel.grid.major=element_blank(),
+                  panel.grid.minor=element_blank(),
+                  panel.border=element_blank(),
+                  axis.line=element_line(),
+                  legend.position="none",
+                  #text=element_text(family='Times'), #Need- https://github.com/wch/extrafont + Ghostscript
+                  #legend.title=element_blank(),
+                  axis.line.x = element_line(color="black", size = 0.9),
+                  axis.line.y = element_line(color="black", size = 0.9),
+                  axis.text.x = element_text(size = 12),
+                  axis.text.y = element_text(size = 12), 
+                  axis.title.x = element_text(size = 12),
+                  axis.title.y = element_text(size = 12))
+
+#Create custom color palette 
+Colors12<-brewer.pal(12,"Paired")
+
+ggplot(gilden2010, aes(x = rt, y = acc, group = sub, color = sub)) +
+  geom_line(aes(y = predict(null.vis)), linetype = 2) + 
+  geom_line(aes(y = vissearch.rmc$model$fitted.values), linetype = 1) + 
+  geom_ribbon(aes(ymin = L1.predict.gilden$lwr,
+                  ymax = L1.predict.gilden$upr,
+                  group = L1.predict.gilden$`gilden2010$sub`,
+                  linetype = NA),
+                  alpha = 0.07) +
+  apatheme + theme(plot.title = element_text(hjust = 0.5)) + 
+  labs(title = "Rmcorr and Random Intercept Multi-Level Model:\n Gilden et al. 2010 Data", x = "Response Time (Seconds)", y = "Accuracy") +
+  geom_point(aes(colour = sub)) +
+  scale_colour_gradientn(colours=Colors12) + 
+  geom_abline(intercept = fixef(null.vis)[1], slope = fixef(null.vis)[2], colour = "black", size = 1, linetype = 2) +
+  
+  scale_y_continuous(breaks=seq(0.80, 1.0, 0.05)) +
+  scale_x_continuous(breaks=seq(0.50, 0.9, 0.1)) 
+
+ggsave(file = "plots/AppendixC_Figure2.eps", width = 5.70 , height = 5.73, dpi = 300)
 
